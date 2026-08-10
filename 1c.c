4c9319fe022240d33c89b02a9fb46e6c7e59d9c3
Mehul-Pangtey <ucss.mehul@gmail.com>
2026-08-10 11:47:46 +0530

    Create 1c.c

---
+++ 1c.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+    while (T--)
+    {
+        int n;
+        scanf("%d", &n);
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            result = result ^ arr[i];
+        }
+
+        printf("%d\n", result);
+    }
+
+    return 0;
+}
